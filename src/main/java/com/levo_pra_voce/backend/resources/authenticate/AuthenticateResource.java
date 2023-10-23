4668cfb42ec09a633d21857ec46b28c3d30e312d
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-10-22 22:33:16 -0300

    feat: create endpoint for register users

--- src/main/java/com/levo_pra_voce/backend/resources/authenticate/AuthenticateResource.java
+++ src/main/java/com/levo_pra_voce/backend/resources/authenticate/AuthenticateResource.java
@@ -1,13 +1,12 @@
 package com.levo_pra_voce.backend.resources.authenticate;
 
+import com.levo_pra_voce.backend.entities.UserType;
+import com.levo_pra_voce.backend.services.authenticate.AuthenticateService;
 import com.levo_pra_voce.backend.services.authenticate.dto.JwtResponseDTO;
+import com.levo_pra_voce.backend.services.authenticate.dto.UserDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.levo_pra_voce.backend.services.authenticate.AuthenticateService;
-import com.levo_pra_voce.backend.services.authenticate.dto.UserAuthenticationDTO;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -16,7 +15,12 @@ public class AuthenticateResource {
     private final AuthenticateService authenticateService;
 
     @GetMapping("/signin")
-    public ResponseEntity<JwtResponseDTO> signin(@RequestBody UserAuthenticationDTO userAuthenticationDTO) {
+    public ResponseEntity<JwtResponseDTO> signin(@RequestBody UserDTO userAuthenticationDTO) {
       return ResponseEntity.ok(authenticateService.signin(userAuthenticationDTO));
+    }
+
+    @PostMapping("/signup/{userType}")
+    public ResponseEntity<JwtResponseDTO> signup(@PathVariable UserType userType, @RequestBody UserDTO userAuthenticationDTO) {
+      return ResponseEntity.ok(authenticateService.signup(userType, userAuthenticationDTO));
     }
 }
