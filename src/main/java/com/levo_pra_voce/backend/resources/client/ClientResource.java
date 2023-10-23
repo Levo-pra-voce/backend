a91867c0ce2158ead107734d06969b6b8b667a5b
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-10-22 22:18:46 -0300

    feat: remove methods in ClientResource

--- src/main/java/com/levo_pra_voce/backend/resources/client/ClientResource.java
+++ src/main/java/com/levo_pra_voce/backend/resources/client/ClientResource.java
@@ -1,11 +1,6 @@
 package com.levo_pra_voce.backend.resources.client;
 
-import com.levo_pra_voce.backend.services.authenticate.dto.JwtResponseDTO;
-import com.levo_pra_voce.backend.services.client.ClientService;
-import com.levo_pra_voce.backend.services.client.dto.UserClientDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/client")
 public class ClientResource {
-
-    private final ClientService clientService;
-
-    @PostMapping("/create/user")
-    public JwtResponseDTO createUser(@RequestBody UserClientDTO userClientDTO) {
-        return this.clientService.createUser(userClientDTO);
-    }
 }
