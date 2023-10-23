8dfe2fd16b4f5c98a0a7ec1d8405110d6539c323
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-10-23 02:16:56 -0300

    feat: create user mock class for mock one delivery user and client user

---
+++ src/test/java/com/levopravoce/backend/user/mock/UserMock.java
@@ -0,0 +1,47 @@
+package com.levopravoce.backend.user.mock;
+
+import com.levopravoce.backend.entities.Address;
+import com.levopravoce.backend.entities.User;
+import com.levopravoce.backend.entities.UserType;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public final class UserMock {
+
+  public static final String CLIENT_EMAIL = "client@gmail.com";
+  public static final String DELIVERY_EMAIL = "delivery@gmail.com";
+  private static final Address ADDRESS =
+      Address.builder()
+          .id(1L)
+          .city("Test")
+          .state("Test")
+          .active(true)
+          .zipCode("123456")
+          .complement("Test")
+          .number("123")
+          .street("Test")
+          .creationDate(LocalDateTime.now())
+          .build();
+
+  public static final User DELIVERY_USER =
+      User.builder()
+          .id(2L)
+          .userType(UserType.ENTREGADOR)
+          .email(DELIVERY_EMAIL)
+          .password("123456")
+          .firstName("Test")
+          .cpf("12345678912")
+          .addresses(List.of(ADDRESS))
+          .build();
+  public static final User CLIENT_USER =
+      User.builder()
+          .id(1L)
+          .userType(UserType.CLIENTE)
+          .email(CLIENT_EMAIL)
+          .password("123456")
+          .firstName("Test")
+          .lastName("Test")
+          .cpf("12345678910")
+          .addresses(List.of(ADDRESS))
+          .build();
+}
