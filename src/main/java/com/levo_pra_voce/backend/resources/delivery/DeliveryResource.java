3731bba7e906a309604a2d62878504632fe74ff9
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-10-22 22:19:01 -0300

    feat: remove methods in DeliveryResource

--- src/main/java/com/levo_pra_voce/backend/resources/delivery/DeliveryResource.java
+++ src/main/java/com/levo_pra_voce/backend/resources/delivery/DeliveryResource.java
@@ -1,11 +1,6 @@
 package com.levo_pra_voce.backend.resources.delivery;
 
-import com.levo_pra_voce.backend.services.authenticate.dto.JwtResponseDTO;
-import com.levo_pra_voce.backend.services.delivery.DeliveryService;
-import com.levo_pra_voce.backend.services.delivery.dto.UserDeliveryDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DeliveryResource {
 
-    private final DeliveryService deliveryService;
-    @PostMapping("/create/user")
-    public JwtResponseDTO createUser(@RequestBody UserDeliveryDTO userDeliveryDTO) {
-        return this.deliveryService.createUser(userDeliveryDTO);
-    }
 }
