1484ee3c311a0b8f84a8b4391a79c54d324e97bc
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-10-23 00:46:48 -0300

    feat: add method for get current user of SecurityContextHolder

--- src/main/java/com/levopravoce/backend/common/SecurityUtils.java
+++ src/main/java/com/levopravoce/backend/common/SecurityUtils.java
@@ -11,4 +11,11 @@ public class SecurityUtils {
         } 
         throw new RuntimeException("User not found");
     }
+
+  public static User getCurrentUser() {
+    if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof User) {
+      return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+    throw new RuntimeException("User not found");
+  }
 }
