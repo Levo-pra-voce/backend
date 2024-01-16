d37b9a22795377f53c72948c9f9514c2df1f93b8
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-15 21:11:51 -0300

    feat: apply auto indentation in UserResource

--- src/main/java/com/levopravoce/backend/resources/user/UserResource.java
+++ src/main/java/com/levopravoce/backend/resources/user/UserResource.java
@@ -3,12 +3,11 @@ package com.levopravoce.backend.resources.user;
 import com.levopravoce.backend.common.SecurityUtils;
 import com.levopravoce.backend.services.authenticate.dto.UserDTO;
 import com.levopravoce.backend.services.user.UserSearchService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,13 +16,13 @@ public class UserResource {
 
   private final UserSearchService userSearchService;
 
-    @GetMapping("/me")
-    public UserDTO getUser() {
+  @GetMapping("/me")
+  public UserDTO getUser() {
     return userSearchService.getUser(SecurityUtils.getCurrentUsername());
-    }
+  }
 
-    @GetMapping("/all")
-    public List<UserDTO> getUserList() {
+  @GetMapping("/all")
+  public List<UserDTO> getUserList() {
     return userSearchService.getUserList();
-    }
+  }
 }
