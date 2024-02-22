813adeb4a09f788322ce1181f6f5529afe99bc5b
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-02-22 01:21:29 -0300

    feat: remove stomp for websocket connection

--- src/main/java/com/levopravoce/backend/config/WebSocketConfiguration.java
+++ src/main/java/com/levopravoce/backend/config/WebSocketConfiguration.java
@@ -1,89 +1,55 @@
 package com.levopravoce.backend.config;
 
 import com.levopravoce.backend.security.JwtTokenUtil;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import java.util.List;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 
 @Configuration
 @EnableWebSocket
-@EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfiguration extends AbstractSecurityWebSocketMessageBrokerConfigurer implements WebSocketMessageBrokerConfigurer  {
-    private final JwtTokenUtil jwtService;
-    private final UserDetailsService userService;
+public class WebSocketConfiguration implements WebSocketConfigurer {
 
-    private final ApplicationContext context;
+  private final JwtTokenUtil jwtService;
+  private final UserDetailsService userService;
+  private final WebSocketHandler webSocketHandler;
+  private final ApplicationContext context;
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(webSocketHandler, "/socket")
+        .addInterceptors(new HttpSessionHandshakeInterceptor() {
+          @Override
+          public boolean beforeHandshake(
+              ServerHttpRequest request,
+              ServerHttpResponse response,
+              org.springframework.web.socket.WebSocketHandler wsHandler,
+              Map<String, Object> attributes) throws java.lang.Exception {
+            return super.beforeHandshake(request, response, wsHandler, attributes)
+                && request.getPrincipal() != null && ((UsernamePasswordAuthenticationToken)
+                request.getPrincipal()).isAuthenticated();
+          }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket").setAllowedOrigins("*");
-    }
+          @Override
+          public void afterHandshake(
+              ServerHttpRequest request,
+              ServerHttpResponse response,
+              org.springframework.web.socket.WebSocketHandler wsHandler,
+              Exception ex) {
+            super.afterHandshake(request, response, wsHandler, ex);
+          }
 
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
-    }
-
-    @Override
-    protected boolean sameOriginDisabled() {
-        return true;
-    }
-
-    @Override
-    protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
-        messages.anyMessage().authenticated();
-    }
-//    @Override
-//    public void configureClientInboundChannel(ChannelRegistration registration) {
-//        registration.interceptors(new ChannelInterceptor() {
-//            @Override
-//            public Message<?> preSend(Message<?> message, MessageChannel channel) {
-//                StompHeaderAccessor accessor =
-//                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-//
-//                if (accessor == null) {
-//                    throw new IllegalArgumentException("Invalid token");
-//                }
-//
-//                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-//                    String authorization = Objects.requireNonNull(accessor.getNativeHeader("X-Authorization")).get(0);
-////                    logger.debug("X-Authorization: {}", authorization);
-//                    final String userEmail = jwtService.getUsernameFromToken(authorization);
-//                    if (StringUtils.isEmpty(userEmail)) {
-//                        throw new IllegalArgumentException("Invalid token");
-//                    }
-//                    final UserDetails userDetails = userService.loadUserByUsername(userEmail);
-//
-//                    if (!jwtService.isTokenValid(authorization, userDetails)) {
-//                        throw new IllegalArgumentException("Invalid token");
-//                    }
-//
-//                    Authentication authentication = new UsernamePasswordAuthenticationToken(
-//                            userDetails, null, userDetails.getAuthorities());
-//                    accessor.setUser(authentication);
-//                }
-//                return message;
-//            }
-//        });
-//    }
+        });
+  }
 }
--- src/main/java/com/levopravoce/backend/resources/chat/ChatSocketResoure.java
+++ src/main/java/com/levopravoce/backend/resources/chat/ChatSocketResoure.java
@@ -3,7 +3,6 @@ package com.levopravoce.backend.resources.chat;
 import com.levopravoce.backend.services.chat.dto.MessageRequestDTO;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
@@ -12,9 +11,6 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class ChatSocketResoure {
-
-    private final SimpMessagingTemplate messagingTemplate;
-    
     @MessageMapping("/chat")
     @SendToUser("/topic/chat")
     public MessageRequestDTO chat(MessageRequestDTO message) {
@@ -23,6 +19,6 @@ public class ChatSocketResoure {
 
     @MessageMapping("/chat/{username}")
     public void chat(MessageRequestDTO message, @DestinationVariable String username) {
-        this.messagingTemplate.convertAndSendToUser(username, "/topic/chat", message);
+//        this.messagingTemplate.convertAndSendToUser(username, "/topic/chat", message);
     }
 }
