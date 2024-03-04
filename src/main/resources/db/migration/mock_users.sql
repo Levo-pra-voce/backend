56fe658fb1279ea6a0a7f3eb026d0518cf0c944c
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-03-04 01:49:11 -0300

    feat: update script for mock users

--- src/main/resources/db/migration/mock_users.sql
+++ src/main/resources/db/migration/mock_users.sql
@@ -1,4 +1,4 @@
-create function mock_usuario() returns void as
+create or replace function mock_usuario() returns void as
 $$
 declare
     i integer;
@@ -6,16 +6,19 @@ begin
     i := 0;
     while i < 500000
         loop
+        -- WITH NAME
+--         insert into usuario (email, senha, nome, tipo, status) values ('test'::text || i::text || '@gmail.com'::text,'$2a$10$RLb6W1yRuqzjurauXUcaB.Yx938k37xSV9UxM8kZUxBXgytPVhoC2','test'::text || i::text, 'CLIENTE', 'ACTIVE');
+        -- WITHOUT NAME
             insert into usuario (email, senha, nome, tipo, status)
             values ('test'::text || i::text || '@gmail.com'::text,
-                    '$2a$10$RLb6W1yRuqzjurauXUcaB.Yx938k37xSV9UxM8kZUxBXgytPVhoC2',
-                    'test'::text || i::text, 'CLIENTE', 'ACTIVE');
+                    '$2a$10$RLb6W1yRuqzjurauXUcaB.Yx938k37xSV9UxM8kZUxBXgytPVhoC2', null, 'CLIENTE',
+                    'ACTIVE');
             i := i + 1;
         end loop;
 end;
 $$ language plpgsql;
 
-create function mock_grupo() returns void as
+create or replace function mock_grupo() returns void as
 $$
 declare
     i integer;
@@ -29,26 +32,12 @@ begin
 end;
 $$ language plpgsql;
 
-do
-$$
-    declare
-        i integer;
-    begin
-        i := 0;
-        while i < 500000
-            loop
-                insert into grupo (nome, ativo) values ('test'::text || i::text, true);
-                i := i + 1;
-            end loop;
-    end
-$$;
 
-
-create function mock_usuario_grupo() returns void as
+create or replace function mock_usuario_grupo() returns void as
 $$
 declare
-    i             integer;
-    base_user_id  integer;
+    i            integer;
+    base_user_id integer;
     base_group_id integer;
 begin
     i := 0;
@@ -56,7 +45,7 @@ begin
     base_group_id := (select min(id) from grupo where nome like 'test%');
     raise notice 'base_user_id: %', base_user_id;
     raise notice 'base_group_id: %', base_group_id;
-    while i < 500000
+    while i < 499999
         loop
             insert into usuario_grupo (id_usuario, id_grupo, ativo)
             values (base_user_id + i, base_group_id + i, true);
@@ -67,12 +56,9 @@ begin
 end;
 $$ language plpgsql;
 
-
--- alter sequence usuario_id_seq restart with 1;
--- alter sequence grupo_id_seq restart with 1;
--- truncate table usuario_grupo;
--- truncate table grupo cascade;
--- truncate table usuario cascade;
--- select mock_usuario();
--- select mock_grupo();
--- select mock_usuario_grupo();
+truncate table usuario_grupo;
+truncate table grupo cascade;
+truncate table usuario cascade;
+select mock_usuario();
+select mock_grupo();
+select mock_usuario_grupo();
