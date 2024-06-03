d9340532ca69cd70a4f73f6d44f041d6d9d6a998
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-06-03 00:43:27 -0300

    feat: apply code for updating delivery user

--- src/main/java/com/levopravoce/backend/repository/VehicleRepository.java
+++ src/main/java/com/levopravoce/backend/repository/VehicleRepository.java
@@ -4,8 +4,21 @@ import com.levopravoce.backend.entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     List<Vehicle> getVehiclesByUserId(Long userId);
+
+    @Modifying
+    @Query(value = """
+        UPDATE public.veiculo set ativo = false
+            WHERE id_usuario = :userId
+""", nativeQuery = true)
+    void disableAllVehiclesByUserId(Long userId);
+
+    boolean existsByRenavam(String renavam);
+
+    boolean existsByPlate(String plate);
 }
