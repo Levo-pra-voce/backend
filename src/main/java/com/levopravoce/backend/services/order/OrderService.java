c82f31a128e8dba779c16cc5c7a46f125c670657
Tr00vuada <gabrielmiguel651@gmail.com>
2024-06-16 04:27:44 -0300

    feat: create methods for find by delivery id and get all deliveries with status in progress or pending

--- src/main/java/com/levopravoce/backend/services/order/OrderService.java
+++ src/main/java/com/levopravoce/backend/services/order/OrderService.java
@@ -5,62 +5,60 @@ import com.levopravoce.backend.entities.Order;
 import com.levopravoce.backend.entities.User;
 import com.levopravoce.backend.entities.UserType;
 import com.levopravoce.backend.repository.OrderRepository;
-import com.levopravoce.backend.services.map.GoogleMapsService;
-import com.levopravoce.backend.services.map.dto.LatLngDTO;
+import com.levopravoce.backend.services.order.dto.OrderDTO;
 import com.levopravoce.backend.services.order.mapper.OrderMapper;
 import com.levopravoce.backend.services.order.utils.OrderUtils;
+import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.levopravoce.backend.services.order.dto.OrderDTO;
-
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-    private final OrderRepository orderRepository;
-    private final OrderMapper orderMapper;
-    private final OrderUtils orderUtils;
-    private final GoogleMapsService googleMapsService;
 
-    public OrderDTO createOrder(OrderDTO orderDTO) {
-        orderUtils.validateNewOrder(orderDTO);
+  private final OrderRepository orderRepository;
+  private final OrderMapper orderMapper;
+  private final OrderUtils orderUtils;
 
-        User currentUser = SecurityUtils.getCurrentUser().orElseThrow();
-        if (!Objects.equals(currentUser.getUserType(), UserType.CLIENTE)) {
-            throw new RuntimeException("Apenas clientes podem criar pedidos.");
-        }
+  public OrderDTO createOrder(OrderDTO orderDTO) {
+    orderUtils.validateNewOrder(orderDTO);
 
-        boolean existsByStatusInProgressOrPending = orderRepository.existsByStatusInProgressOrPending(currentUser.getId());
-        if (existsByStatusInProgressOrPending) {
-            throw new RuntimeException("Você já possui um pedido em andamento ou pendente.");
-        }
-
-        var result = googleMapsService.getDistance(LatLngDTO.builder()
-                        .lat(orderDTO.getOriginLatitude())
-                        .lng(orderDTO.getOriginLongitude())
-                        .build(),
-                LatLngDTO.builder()
-                        .lat(orderDTO.getDestinationLatitude())
-                        .lng(orderDTO.getDestinationLongitude())
-                        .build());
-        if(result == null){
-            throw new IllegalArgumentException("Erro ao buscar distância da API do Google Maps.");
-        }
-
-        if (result.getDistanceValueMeters() == null) {
-            throw new IllegalArgumentException("Origem e destino são inválidos.");
-        }
-
-        Order order = orderMapper.toEntity(orderDTO);
-        order.setDistanceMeters(result.getDistanceValueMeters());
-        order.setDurationSeconds(result.getDurationValueSeconds());
-        order.setDestinationAddress(result.getDestinationAddress());
-        order.setOriginAddress(result.getOriginAddress());
-        order.setClient(currentUser);
-        return orderMapper.toDTO(orderRepository.save(order));
+    User currentUser = SecurityUtils.getCurrentUser().orElseThrow();
+    if (!Objects.equals(currentUser.getUserType(), UserType.CLIENTE)) {
+      throw new RuntimeException("Apenas clientes podem criar pedidos.");
     }
 
+    boolean existsByStatusInProgressOrPending = orderRepository.existsByStatusInProgressOrPending(
+        currentUser.getId());
+    if (existsByStatusInProgressOrPending) {
+      throw new RuntimeException("Você já possui um pedido em andamento ou pendente.");
+    }
 
+    Order order = orderMapper.toEntity(orderDTO);
+    order.setClient(currentUser);
+    return orderMapper.toDTO(orderRepository.save(order));
+  }
 
+  public List<OrderDTO> getDeliveriesPending(
+      User currentUser
+  ) {
+    if (!Objects.equals(currentUser.getUserType(), UserType.ENTREGADOR)) {
+      throw new RuntimeException("Apenas entregadores podem visualizar pedidos pendentes.");
+    }
+
+    List<Order> orders = orderRepository.findByStatusPendingOrInProgress(currentUser.getId());
+    List<OrderDTO> orderDTOS = orders.stream().map(orderMapper::toDTO).toList();
+    return orderDTOS;
+  }
+
+  public OrderDTO getOrderById(User currentUser, Long id) {
+    Order order = orderRepository.findById(id).orElseThrow();
+
+    if (!currentUser.getId().equals(order.getDeliveryman().getId())) {
+      throw new RuntimeException("Você não tem permissão para visualizar este pedido.");
+    }
+
+    return orderMapper.toDTO(order);
+  }
 }
