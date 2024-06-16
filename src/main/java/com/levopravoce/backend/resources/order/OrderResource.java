0401fa18c3d6cfa99fb806156bdcadf519db7565
Tr00vuada <gabrielmiguel651@gmail.com>
2024-06-16 04:27:52 -0300

    feat: create endpoints for find by delivery id and get all deliveries with status in progress or pending

--- src/main/java/com/levopravoce/backend/resources/order/OrderResource.java
+++ src/main/java/com/levopravoce/backend/resources/order/OrderResource.java
@@ -1,8 +1,13 @@
 package com.levopravoce.backend.resources.order;
 
+import com.levopravoce.backend.common.SecurityUtils;
+import com.levopravoce.backend.entities.User;
 import com.levopravoce.backend.services.order.OrderService;
 import com.levopravoce.backend.services.order.dto.OrderDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +27,15 @@ public class OrderResource {
         return this.orderService.createOrder(orderDTO);
     }
 
+    @GetMapping("/deliveries-pending")
+    public List<OrderDTO> getDeliveriesPending() {
+        User currentUser = SecurityUtils.getCurrentUserThrow();
+        return this.orderService.getDeliveriesPending(currentUser);
+    }
+
+    @GetMapping("/{id}")
+    public OrderDTO getOrderById(@PathVariable  Long id) {
+        User currentUser = SecurityUtils.getCurrentUserThrow();
+        return this.orderService.getOrderById(currentUser, id);
+    }
 }
