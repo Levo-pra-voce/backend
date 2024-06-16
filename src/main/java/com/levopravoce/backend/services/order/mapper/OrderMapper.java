55da36030ac8d78432eb54046681b161743d93dd
Tr00vuada <gabrielmiguel651@gmail.com>
2024-06-16 04:25:50 -0300

    feat: add new fields to OrderMapper

--- src/main/java/com/levopravoce/backend/services/order/mapper/OrderMapper.java
+++ src/main/java/com/levopravoce/backend/services/order/mapper/OrderMapper.java
@@ -12,6 +12,9 @@ public interface OrderMapper {
     @Mapping(target = "width", source = "width")
     @Mapping(target = "maxWeight", source = "maxWeight")
     @Mapping(target = "haveSecurity", source = "haveSecurity")
+    @Mapping(target = "status", source = "status")
+    @Mapping(target = "destinationAddress", source = "destinationAddress")
+    @Mapping(target = "originAddress", source = "originAddress")
     Order toEntity(OrderDTO orderDTO);
     @InheritInverseConfiguration
     OrderDTO toDTO(Order order);
