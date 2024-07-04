39bff0e59421ac443ac901a990180681a49098c8
Tr00vuada <gabrielmiguel651@gmail.com>
2024-07-04 09:35:39 -0300

    test: add display name to OrderUtilsTest

--- src/test/java/com/levopravoce/backend/order/OrderUtilsTest.java
+++ src/test/java/com/levopravoce/backend/order/OrderUtilsTest.java
@@ -4,6 +4,7 @@ import com.levopravoce.backend.services.order.dto.OrderDTO;
 import com.levopravoce.backend.services.order.utils.OrderUtils;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,71 +20,83 @@ public class OrderUtilsTest {
     private final OrderUtils orderUtils;
 
     @Test
+    @DisplayName("A altura não pode ser negativa")
     public void givenOrderDTOWithNegativeWidth_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double width = -1.0;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateWidth(width));
     }
 
     @Test
+    @DisplayName("A largura não pode ser zero")
     public void givenOrderDTOWithZeroWidth_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double width = 0.0;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateWidth(width));
     }
 
     @Test
+    @DisplayName("A largura não pode ser nula")
     public void givenOrderDTOWithNullWidth_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double width = null;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateWidth(width));
     }
 
     @Test
+    @DisplayName("A altura não pode ser negativa")
     public void givenOrderDTOWithNegativeHeight_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double height = -1.0;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateHeight(height));
     }
 
     @Test
+    @DisplayName("A altura não pode ser zero")
     public void givenOrderDTOWithZeroHeight_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double height = 0.0;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateHeight(height));
     }
 
     @Test
+    @DisplayName("A altura não pode ser nula")
     public void givenOrderDTOWithNullHeight_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double height = null;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateHeight(height));
     }
 
     @Test
+    @DisplayName("O peso máximo não pode ser negativo")
     public void givenOrderDTOWithNegativeMaxWeight_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double maxWeight = -1.0;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validadeMaxWeight(maxWeight));
     }
 
     @Test
+    @DisplayName("O peso máximo não pode ser zero")
     public void givenOrderDTOWithZeroMaxWeight_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double maxWeight = 0.0;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validadeMaxWeight(maxWeight));
     }
 
     @Test
+    @DisplayName("O peso máximo não pode ser nulo")
     public void givenOrderDTOWithNullMaxWeight_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         Double maxWeight = null;
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validadeMaxWeight(maxWeight));
     }
 
     @Test
+    @DisplayName("A data de entrega não pode ser anterior a data atual")
     public void givenOrderDTOWithPastDeliveryDate_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateDeliveryDate(
             LocalDate.now().minusDays(1)));
     }
 
     @Test
+    @DisplayName("A data de entrega não pode ser nula")
     public void givenOrderDTOWithNullDeliveryDate_whenValidateOrderFields_thenThrowIllegalArgumentException() {
         assertThrows(IllegalArgumentException.class, () -> orderUtils.validateDeliveryDate(null));
     }
 
     @Test
+    @DisplayName("A validação total da ordem não deve lançar exceção")
     public void givenOrderDTOWithValidFields_whenValidateOrderFields_thenDoNothing() {
         OrderDTO orderDTO = OrderDTO.builder()
                 .height(1.0)
